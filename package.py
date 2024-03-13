56710c4f0b3359f6b5534d9b90ee07c93817e438
Marcelo Sercheli <msercheli@gmail.com>
2024-03-13 15:27:29 -0400

    Add rez files

---
+++ package.py
@@ -0,0 +1,51 @@
+name = 'clew'
+
+version = '1.0.0'
+
+authors = [
+    'Martijn Berger',
+]
+
+description = '''OpenCL wrapper'''
+
+with scope('config') as c:
+    import os
+    c.release_packages_path = os.environ['SSE_REZ_REPO_RELEASE_EXT']
+
+requires = [
+]
+
+private_build_requires = [
+]
+
+variants = [
+]
+
+def pre_build_commands():
+    info = {}
+    with open("/etc/os-release", 'r') as f:
+        for line in f.readlines():
+            if line.startswith('#'):
+                continue
+            line_info = line.replace('\n', '').split('=')
+            if len(line_info) != 2:
+                continue
+            info[line_info[0]] = line_info[1].replace('"', '')
+    linux_distro = info.get("NAME", "centos")
+    print("Using Linux distro: " + linux_distro)
+
+    if linux_distro.lower().startswith("centos"):
+        command("source /opt/rh/devtoolset-6/enable")
+    elif linux_distro.lower().startswith("rocky"):
+        pass
+
+
+def commands():
+    env.REZ_CLEW_ROOT = '{root}'
+    env.CLEW_LOCATION = '{root}'
+    env.CLEW_INCLUDE_DIR = "{root}/include"
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+    env.LD_LIBRARY_PATH.append("{root}/lib64")
+
+uuid = 'repository.clew'
+
